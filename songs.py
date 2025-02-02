df96f3c7c0a85ee2581d93b7945bd690d356e772
ViiiGrando <162319269+ViiiGrando@users.noreply.github.com>
2025-02-02 10:28:27 -0300

    Add files via upload

---
+++ songs.py
@@ -0,0 +1,22 @@
+key = input('Type a key to play a note').upper()
+
+if key == 'A':
+    pass
+elif key == 'B':
+    pass
+elif key == 'C':
+    pass
+
+elif key == 'D':
+    pass
+
+elif key == 'E':
+    pass
+
+elif key == 'F':
+    pass
+
+elif key == 'G':
+    pass
+
+
